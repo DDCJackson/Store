32c31bf437e6ea68b80ff412fbe3fe823efaaa17
张秀峰 <jackson.zhang@daydaycook.com>
2017-10-18 13:50:16 +0800

    添加显示支付方式页面

---
+++ DDC_Store/Main/CreateContract/Controllers/FinishContractViewController.m
@@ -0,0 +1,40 @@
+//
+//  FinishContractViewController.m
+//  DDC_Store
+//
+//  Created by 张秀峰 on 2017/10/18.
+//  Copyright © 2017年 DDC. All rights reserved.
+//
+
+#import "FinishContractViewController.h"
+
+@interface FinishContractViewController ()
+
+@end
+
+@implementation FinishContractViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
