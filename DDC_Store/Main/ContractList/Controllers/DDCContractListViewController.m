db5fb32797d1231dd887b9bcca20b4d2c7c87f1c
DayDayCook <ddcclouddev@daydaycook.com>
2017-10-22 20:22:29 +0800

    首页footer

--- DDC_Store/Main/ContractList/Controllers/DDCContractListViewController.m
+++ DDC_Store/Main/ContractList/Controllers/DDCContractListViewController.m
@@ -66,6 +66,7 @@
 - (void)viewWillAppear:(BOOL)animated
 {
     _page = 0;
+    [self.view.collectionHolderView.collectionView setFooterHidden:NO];
     [self reloadPage];
     [super viewWillAppear:animated];
     self.navigationController.navigationBarHidden = YES;
